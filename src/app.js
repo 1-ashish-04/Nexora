0725e41d926f8f4389e1c6143a932d5f97de3276
1-ashish-04 <ashish4.jayaswal@gmail.com>
2026-02-27 13:44:36 +0530

    HTTP routes

--- src/app.js
+++ src/app.js
@@ -2,18 +2,35 @@ const express = require("express")
 
 const app = express()
 
-app.use("/test",(req, res)=> {
-   res.send("Server is started, Nexora (Next Developer) is on the way...!")
+// only handle GET call
+app.get("/user", (req,res)=> {
+    res.send({firstName: "Ashish", lastName : "Jayaswal"})
 })
 
-app.use("/hello",(req, res)=> {
-   res.send("Hello Nexorians, ...! Enjo the NEXORA.")
+// only handle POST call
+app.post("/user", (req,res)=>{
+    res.send("User Data Successfully saved into db")
 })
 
-app.use("/", (req,res)=>{
-    res.send("Home page ........ find your match")
+// only handle DELETE call
+app.delete("/user",(req,res)=>{
+    res.send("User data deleted successfully from the db")
 })
 
+// only handle PUT call
+app.put('/user',(req,res)=>{
+    res.send("User Data updated through put")
+})
+
+// only handle PATCH call
+app.patch('/user',(req,res)=>{
+    res.send("User detail updated through patch")
+})
+
+// can handle anytype of the call (GET, POST, PATCH, PUT, DELETE)
+app.use('/', (req,res) => {
+    res.send("Home Page, can handle anyone")
+})
 const portNum = 7777
 app.listen(portNum , ()=> {
     console.log("Server is listening on port number", portNum)
