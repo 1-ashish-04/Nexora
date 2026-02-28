eb52d67e375ed5a58683ecd6784cd078bdf6b137
1-ashish-04 <ashish4.jayaswal@gmail.com>
2026-02-28 18:04:28 +0530

    wild-card-error-handling

--- src/app.js
+++ src/app.js
@@ -2,31 +2,22 @@ const express = require("express");
 
 const app = express();
 
-const {adminAuth, userAuth}  = require("./middlewares/auth")
-
-app.use("/admin", adminAuth)
-
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("All data loaded successfully.")
+app.use("/user", (req,res)=>{
+    try{
+    throw new Error("Error Happend")
+    res.send("Helllloooo User...")}
+    catch{
+        res.status(500).send("Some went wrong.")
+    }
 })
 
-app.get("/admin/deleteUser", (req,res)=>{  
-    res.send("Specific data deleted successfully.")  
+/*
+app.use("/", (err, req, res, next)=>{
+    if(err){
+        res.status(500).send("Something went wrong")
+    }
 })
-
-app.use("/user/login", (req,res)=>{
-    res.send("User loggin successfully.")
-})
-
-app.use("/user", userAuth)
-
-app.get("/user/data", (req,res)=>{
-    res.send("User get its data successfully.")
-})
-
-app.get("/user/delete", (req,res)=>{
-    res.send("User data deleted.")
-})
+    */
 
 const portNum = 7777;
 app.listen(portNum, () => {
