657f91af3374cf8681345f685553bc3eff5342fc
1-ashish-04 <ashish4.jayaswal@gmail.com>
2026-03-01 16:43:31 +0530

    Remove database.js from tracking

--- src/config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-// it always return promise
-const connectDB = async ()=>{
-await mongoose.connect("mongodb+srv://FirstNode:F1rstN0de@cluster0.ss1b3v1.mongodb.net/nexora")
-}
-
-module.exports = {connectDB}
-
